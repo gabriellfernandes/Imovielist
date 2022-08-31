ee449dd10579afa45bbe2efb5e3eb50c7456addd
Arthur <arthur13042001@gmail.com>
2022-08-31 11:24:03 -0300

    Feature: Component Header created

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,42 @@
+import { HeaderDiv } from "./style";
+import {BiMovie} from 'react-icons/bi'
+import {AiFillHome, AiFillCompass, AiFillStar, AiFillBell} from 'react-icons/ai'
+import {FaUser} from 'react-icons/fa'
+export default function Header (){
+
+
+
+    return (
+        
+        <HeaderDiv>
+            
+                <div>
+               <BiMovie className="icon" color="#9A9DA5" size={26}/>
+               <span>MyMovieList</span>
+                </div>
+
+                <div>
+                    <div>
+                    <AiFillHome className="icon"  size={26}/>
+                    <span>home</span>
+                    </div>
+
+                    <div>
+                    <AiFillCompass className="icon"   size={26}/>
+                    <span>Discover</span>
+                    </div>
+
+                    <div>
+                    <AiFillStar className="icon"  size={26}/>
+                    <span>Top-Rated</span>
+                    </div>
+                    <AiFillBell className="icon"  size={26}/>
+                    <FaUser className="icon"  size={24}/>
+                </div>
+
+
+
+=
+        </HeaderDiv>
+    )
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,25 @@
+import Header from "../../components/Header";
+import {BiMovie} from 'react-icons/bi'
+import { HeaderDiv } from "../../components/Header/style";
+
+
+
+interface IHeaderProps{
+
+}
+
+
+
+export default function Home(){
+
+
+
+
+
+    return (
+        <Header/>
+
+
+
+    )
+}

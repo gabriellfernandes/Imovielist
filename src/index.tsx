24ba1254e1a2c6ae0261e502cb07a34c3ed7f71f
artorys <darksoulsgui123@gmail.com>
2022-09-05 10:29:51 -0300

    feat : add card hover video

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,6 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
       <ToastContainer/>
       <Provider>
@@ -23,7 +22,6 @@ root.render(
       </Provider>
       <Reset/>
     </BrowserRouter>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/interfaces/axiosReponseApiTmdb.ts
+++ src/interfaces/axiosReponseApiTmdb.ts
@@ -1,3 +1,20 @@
+export interface IVideos
+{
+  id : number;
+  results :
+  [{
+    iso_639_1 : string
+    iso_3166_1 : string
+    name : string;
+    key : string;
+    site : string;
+    size : number;
+    type : string;
+    official : boolean;
+    published_at : string;
+    id : string;
+  }]
+}
 export interface IRated
 {
     page: number;
@@ -40,6 +57,7 @@ export interface IResults
     video: boolean;
     vote_average: number;
     vote_count: number
+    url : string | Promise<string>
 }
 
 export interface IResponsePopularMovie{
